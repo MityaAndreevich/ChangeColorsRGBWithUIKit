4208ad98807d19b9da38f1a6fb2e71f29d23c9d3
MityaAndreevich <dmitry.logachev@icloud.com>
2021-09-04 00:14:25 +0300

    new class added for starting view

---
+++ ChangeColorsRGBWithUIKit/ViewControllers/StartingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  StartingViewController.swift
+//  ChangeColorsRGBWithUIKit
+//
+//  Created by Дмитрий Логачёв on 04.09.2021.
+//
+
+import UIKit
+
+class StartingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+}
